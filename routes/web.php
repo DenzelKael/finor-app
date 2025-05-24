5bcc1b390a3227af2d789ddad025306f5c60f8c2
Victor Manuel Vargas Chavez <zedequis@gmail.com>
2025-05-24 09:53:10 -0400

    Commit inicial

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
